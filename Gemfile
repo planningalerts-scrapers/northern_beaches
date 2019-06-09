342a74697c54655054821e80b4efc0fbdc31d108
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 02:56:20 +1000

    Switch over to using gem

--- Gemfile
+++ Gemfile
@@ -2,8 +2,9 @@ source 'https://rubygems.org'
 
 ruby "~>2.4"
 
+# gem "icon_scraper", git: "https://github.com/planningalerts-scrapers/icon_scraper.git"
+gem "icon_scraper", path: "../icon_scraper"
 gem 'scraperwiki', git: 'https://github.com/openaustralia/scraperwiki-ruby.git', branch: 'morph_defaults'
-gem 'mechanize'
 
 gem "vcr"
 gem "webmock"
--- scraper.rb
+++ scraper.rb
@@ -1,60 +1,10 @@
-require 'scraperwiki'
-require 'mechanize'
+require "icon_scraper"
 
-starting_url = 'https://eservices.northernbeaches.nsw.gov.au/ePlanning/live/Public/XC.Track/SearchApplication.aspx?d=last14days&k=LodgementDate&t=DevApp'
-
-def clean_whitespace(a)
-  a.gsub("\r", ' ').gsub("\n", ' ').squeeze(" ").strip
+IconScraper.scrape_with_params(
+  url: "https://eservices.northernbeaches.nsw.gov.au/ePlanning/live/Public",
+  period: "last14days",
+  types: ["DevApp"]
+) do |record|
+  record["address"] = record["address"].gsub(",", "")
+  IconScraper.save(record)
 end
-
-def extract_addresses!(bits)
-  addresses = []
-  bits.each_with_index do |line, index|
-    if /\r\n\t\t\t\t\t\t\t\t\tAddress:\r\n\t\t\t\t\t\t\t\t\t <strong>(.*)<\/strong>/.match(line)
-      addresses << clean_whitespace(line.match(/<strong>(.*)<\/strong>/)[1])
-
-      bits.delete_at(index)
-    end
-  end
-  addresses
-end
-
-def extract_date!(bits)
-  date  = nil
-  bits.each_with_index do |line, index|
-    if /(\d\d)\/(\d\d)\/(\d\d\d\d)/.match(line)
-      m = line.match(/(\d\d)\/(\d\d)\/(\d\d\d\d)/)
-      date = Date.new(m[3].to_i, m[2].to_i, m[1].to_i).to_s
-
-      bits.delete_at(index)
-    end
-  end
-  date
-end
-
-def scrape_table(doc)
-
-  doc.search('.result')[1..-1].each do |tr|
-    bits = tr.to_s.split("<br>")
-    addresses = extract_addresses!(bits) # modifies bits to remove address lines from array
-    date_received = extract_date!(bits)
-
-    record = {
-      'council_reference' => clean_whitespace(tr.at('a').inner_text),
-      'description'       => clean_whitespace(bits[2]),
-      'date_received'     => date_received,
-      'address'           => addresses.first,
-      'date_scraped'      => Date.today.to_s,
-      'info_url'          => (doc.uri + tr.at('a')['href']).to_s
-    }
-
-    puts "Saving record " + record['council_reference'] + " - " + record['address']
-#       puts record
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  end
-end
-
-agent = Mechanize.new
-doc = agent.get(starting_url)
-
-scrape_table(doc)
